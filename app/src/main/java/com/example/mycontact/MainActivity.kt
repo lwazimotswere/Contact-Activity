eb6d118e83bd56bd9a13f9bed2ed10a06c148acb
lwazimotswere <motswerelwazi@gmail.com>
2026-03-03 11:23:55 +0200

    new version

--- app/src/main/java/com/example/mycontact/MainActivity.kt
+++ app/src/main/java/com/example/mycontact/MainActivity.kt
@@ -10,11 +10,34 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
+        val firstName = "Lwazi"
+        val lastName = "Motswere"
+        val address = "8 Ice Cream Road, Rondebosch"
+        val isMale = "Yes"
+        val dayOfBirth = "19"
+        val monthOfBirth = "July"
+        val yearOfBirth = "2005"
+
+        printIn ("Hi my name is", firstName, lastName, "I live in", address)
+
         setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+
+
+    }
+
+    private fun printIn(
+        string: String,
+        firstName: String,
+        lastName: String,
+        string4: String,
+        address: String
+    ) {
     }
 }
